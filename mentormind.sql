147382480581191d7dc0aeba3c203f1b0674979d
SHIV GAWALI <109132393+gawalishankar@users.noreply.github.com>
2024-09-10 11:38:51 +0530

    Add files via upload

---
+++ mentormind.sql
@@ -0,0 +1,41 @@
+create database protocoanpc7698gaurav;
+use protocoanpc7698gaurav;
+
+#create table user 
+
+create table UserDetails(
+id int primary key auto_increment,
+name varchar(30) not null,
+email varchar(30) not null unique,
+password varchar(10) not null,
+phone bigint(10) not null unique
+);
+
+
+#create table appointment
+create table appointment(
+aid int primary key auto_increment,
+patientName varchar(30) not null,
+patientPhone bigint(11) not null,
+patientAge int(3) not null, 
+patientGender varchar(10) not null,
+BloodGroup varchar(10),
+appointmentdate date not null,
+appointmenttime time, 
+addr varchar(50) not null,
+patientEmail varchar(30) not null
+);
+
+alter table appointment add Column doctorName varchar(30) not null;
+alter table appointment add Column doctorPhone bigint(11);
+alter table appointment add Column testName varchar(30) not null;
+
+#create table TestDetails
+create table TestDetails(
+tid int primary key auto_increment,
+testName varchar(30) not null,
+price decimal not null,
+testdate date not null,
+appointmentId int,
+foreign key(appointmentId) references appointment(aid)
+);
